3e38dc95ee62ebb9a5905444edccf38029183990
Artyom <artyomorel707@gmail.com>
2021-02-14 19:25:57 +0300

    feat(Bookshop.DataAccess.MSSQL):Update  models

--- Bookshop.DataAccess.MSSQL/Migrations/20210214162407_AddShowcaseIdToBook.Designer.cs
+++ Bookshop.DataAccess.MSSQL/Migrations/20210214162407_AddShowcaseIdToBook.Designer.cs
@@ -4,14 +4,16 @@ using Bookshop.DataAccess.MSSQL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bookshop.DataAccess.MSSQL.Migrations
 {
     [DbContext(typeof(BookshopContext))]
-    partial class BookshopContextModelSnapshot : ModelSnapshot
+    [Migration("20210214162407_AddShowcaseIdToBook")]
+    partial class AddShowcaseIdToBook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Bookshop.Domain/Models/Book.cs
+++ Bookshop.Domain/Models/Book.cs
@@ -6,5 +6,6 @@
         public string Name { get; set; }
         public int Size { get; set; }
         public decimal Price { get; set; }
+        public int ShowcaseId { get; set; }
     }
 }
