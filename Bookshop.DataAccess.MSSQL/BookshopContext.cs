a02aaff6631c6b8455d8ff5882d00583ea022b85
Artyom <artyomorel707@gmail.com>
2021-02-14 18:52:29 +0300

    Feat ( MSSQL) : Add MSSQL

---
+++ Bookshop.DataAccess.MSSQL/BookshopContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookshop.DataAccess.MSSQL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshop.DataAccess.MSSQL
+{
+    public class BookshopContext: DbContext
+    {
+        public BookshopContext(DbContextOptions<BookshopContext> options):base(options)
+        { }
+        
+        
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Showcase> Showcases { get; set; }
+    }
+}
